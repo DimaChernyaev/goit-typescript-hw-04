756ae67f248ee6ff3fc072742bd68c1d5b0f06eb
Dima_ <chernyaev603@gmail.com>
2023-10-29 19:47:42 +0200

    all lesson

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -1,13 +1,31 @@
-import React, { createContext, useMemo, useState, useContext } from "react";
+import React, {
+  createContext,
+  useMemo,
+  useState,
+  useContext,
+  ReactNode,
+} from "react";
 import noop from "lodash/noop";
 
 type MenuIds = "first" | "second" | "last";
 type Menu = { id: MenuIds; title: string };
 
+type SelectedMenu = {
+  id: MenuIds;
+};
+
+type MenuSelected = {
+  selectedMenu: SelectedMenu;
+};
+
+type MenuAction = {
+  onSelectedMenu: (object: SelectedMenu) => void;
+};
+
 // Додати тип Menu Selected
 
 const MenuSelectedContext = createContext<MenuSelected>({
-  selectedMenu: {},
+  selectedMenu: { id: "first" },
 });
 
 // Додайте тип MenuAction
@@ -17,12 +35,14 @@ const MenuActionContext = createContext<MenuAction>({
 });
 
 type PropsProvider = {
-  children; // Додати тип для children
+  children: ReactNode; // Додати тип для children
 };
 
-function MenuProvider({ children }: PropsProvider) {
+function MenuProvider({ children }: PropsProvider): ReactNode {
   // Додати тип для SelectedMenu він повинен містити { id }
-  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({});
+  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({
+    id: "second",
+  });
 
   const menuContextAction = useMemo(
     () => ({
@@ -48,10 +68,10 @@ function MenuProvider({ children }: PropsProvider) {
 }
 
 type PropsMenu = {
-  menus; // Додайте вірний тип для меню
+  menus: Menu[]; // Додайте вірний тип для меню
 };
 
-function MenuComponent({ menus }: PropsMenu) {
+function MenuComponent({ menus }: PropsMenu): ReactNode {
   const { onSelectedMenu } = useContext(MenuActionContext);
   const { selectedMenu } = useContext(MenuSelectedContext);
 
@@ -67,7 +87,7 @@ function MenuComponent({ menus }: PropsMenu) {
   );
 }
 
-export function ComponentApp() {
+export function ComponentApp(): ReactNode {
   const menus: Menu[] = [
     {
       id: "first",
